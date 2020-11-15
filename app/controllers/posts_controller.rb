81259139ffd3eb5c9f3dce64547cd38d5b757035
kotaro0217 <kotaro96480321@gamil.com>
2020-11-15 17:39:31 +0900

    既読機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,5 +6,16 @@ class PostsController < ApplicationController
   def create
     Post.create(content: params[:content])
     redirect_to action: :index
+
+    def checkde
+      post = Post.find(params[:id])
+      if post.checked
+        post.update(checked: false)
+      else
+        post.update(checked: ture)
+      end
+
+      item = Post.gind(params[:id])
+      render json: {post: item}
   end
 end
